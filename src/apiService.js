50237f536a4f49cd2b5138fc4a768b38c7cf59ae
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2020-11-19 16:04:10 +0200

    добавляет выпонление домашки

--- src/apiService.js
+++ src/apiService.js
@@ -1,6 +1,35 @@
+const API_KEY = '9056490-f67ac46e342f0369ec0e655c9';
+const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal'
 
-export default function fetchCountries(searchQuery) {
-  return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=9056490-f67ac46e342f0369ec0e655c9`).then(response => {
-          return response.json();
-       })
-};
+export default class ImagesApiServis{
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+   }
+  
+  fetchImages() {
+    return fetch(`${BASE_URL}&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`)
+      .then(response => response.json()).then(({hits}) => {
+        this.incrementPage();
+          return hits;
+        });
+    }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+  
+}
+
--- src/index.js
+++ src/index.js
@@ -1,33 +1,65 @@
-import debounce from 'lodash.debounce';
-import imagesAPI from '../src/apiService';
+import ImagesApiService from '../src/apiService';
 import imgCard from '../src/templates/photoCardTpl.hbs';
 
 const refs = {
     cardConteiner: document.querySelector('.gallery'),
-    inputEl: document.querySelector('.js-search-input')
+    searchFormEl: document.querySelector('.search-form'),
+    btnEl: document.querySelector('[data-action="load-more"]')
 }
 
-let searchQuery = '';
+const imagesApiService = new ImagesApiService();
 
-refs.inputEl.addEventListener(
-  'input',
-  debounce(() => {
-    onSearch();
-  }, 1000),
-);
 
-function onSearch() {
-  searchQuery = refs.inputEl.value;
-  imagesAPI(searchQuery)
-    .then(createCountryCard)
-    .catch(error => console.log(error));
+refs.searchFormEl.addEventListener('submit', onSearch);
+refs.btnEl.addEventListener('click', onBtnClick);
+refs.btnEl.disabled = true;
+
+
+function onBtnClick() {
+
+  fetchImgs().then(
+      setTimeout(() => {
+        window.scrollTo({
+          top: document.body.scrollHeight - 800,
+          behavior: 'smooth',
+        });
+      }, 1500),
+    )
+    .catch(err => console.log(err));
+
 }
 
-function renderMarkup(templates, value) {
-    const markup = templates(value);
-    refs.cardConteiner.innerHTML = markup;
+function fetchImgs(images) {
+  
+  return imagesApiService.fetchImages().then(images => {
+    renderMarkup(images)
+    if (images.length < 12) {
+      refs.btnEl.disabled = true;
+    }
+    if (images.length === 0) {
+       alert('Nothing found for your request')
+    }
+  });
+  
 }
 
-function createCountryCard(value) {
-    renderMarkup(imgCard, value);
+function onSearch(e) {
+  e.preventDefault();
+  claerCardContainer();
+  refs.btnEl.disabled = false;
+  imagesApiService.query = e.currentTarget.elements.query.value;
+  imagesApiService.resetPage();
+  if (imagesApiService.query === '') {
+    refs.btnEl.disabled = true;
+    return alert('Enter your query')
+   }
+  fetchImgs();
+}
+
+function renderMarkup(images) {
+    refs.cardConteiner.insertAdjacentHTML('beforeend', imgCard(images));
+}
+
+function claerCardContainer() {
+  refs.cardConteiner.innerHTML = '';
 }
